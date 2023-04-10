0b363462dac40e3c6e13884b182c526c9d128072
Du-Previatti <dudupreviatti@gmail.com>
2023-04-10 12:44:34 -0300

    update_1.0

--- js/events.js
+++ js/events.js
@@ -1,27 +1,22 @@
-async function wait() {
+async function fetchEventos() {
+  try {
     const response = await fetch('https://soundgarden-api.vercel.app/events');
     const data = await response.json();
-    exibirEventos(data);
+
+    const container = document.querySelector('.container.d-flex');
+
+    container.innerHTML = data.map(evento => `
+      <article class="evento card p-5 m-3">
+        <h2>${evento.name} - ${new Date(evento.scheduled).toLocaleDateString()}</h2>
+        <h4>${evento.attractions.join(', ')}</h4>
+        <p>${evento.description}</p>
+        <a href="#" class="btn btn-primary">reservar ingresso</a>
+      </article>
+    `).join('');
+
+  } catch (error) {
+    console.error(error);
   }
-  
-  function exibirEventos(eventos) {
-    function formatarData(data) {
-      const dia = data.getDate().toString().padStart(2, '0');
-      const mes = (data.getMonth() + 1).toString().padStart(2, '0');
-      const ano = data.getFullYear().toString();
-      return `${dia}/${mes}/${ano}`;
-    }
-  
-    eventos.map((item) => {
-      window.document.body.innerHTML += `
-        <article class="evento card p-5 m-3">
-          <h2>${item.name} - ${formatarData(new Date(item.scheduled))}</h2>
-          <h4>${item.attractions}</h4>
-          <p>${item.description}</p>
-          <a href="#" class="btn btn-primary">reservar ingresso</a>
-        </article>
-      `;
-    });
-  }
-  
-  wait();
+}
+
+fetchEventos();
