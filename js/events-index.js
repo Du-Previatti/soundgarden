b1de5113ac4e3486a103309d6755113505fd8c63
Du-Previatti <dudupreviatti@gmail.com>
2023-04-17 23:08:16 -0300

    Correção events-index.js

--- js/events-index.js
+++ js/events-index.js
@@ -1,34 +1,44 @@
+const API_URL = 'https://soundgarden-api.vercel.app/events';
+const CONTAINER_SELECTOR = '.container.d-flex';
+const EVENT_SELECTOR = '.evento';
+
 async function fetchEventos() {
-    try {
-      const response = await fetch('https://soundgarden-api.vercel.app/events');
-      const data = await response.json();
-  
-      const container = document.querySelector('.container.d-flex');
-  
-      // Verifica se o container já contém elementos
-      const eventosAnteriores = container.querySelectorAll('.evento');
-      if (eventosAnteriores.length > 0) {
-        // Remove os elementos anteriores do container
-        eventosAnteriores.forEach(evento => evento.remove());
-      }
-  
-      // Obtém somente os três primeiros eventos
-      const tresPrimeirosEventos = data.slice(0, 3);
-  
-      // Adiciona os novos elementos ao container
-      container.insertAdjacentHTML('beforeend', tresPrimeirosEventos.map(evento => `
-        <article class="evento card p-5 m-3">
-          <h2>${evento.name} - ${new Date(evento.scheduled).toLocaleDateString()}</h2>
-          <h4>${evento.attractions.join(', ')}</h4>
-          <p>${evento.description}</p>
-          <a href="" class="btn btn-primary">reservar ingresso</a>
-        </article>
-      `).join(''));
-  
-    } catch (error) {
-      console.error(error);
+  try {
+    const response = await fetch(API_URL);
+    if (!response.ok) {
+      throw new Error('Não foi possível carregar os eventos.');
     }
+    const data = await response.json();
+
+    const container = document.querySelector(CONTAINER_SELECTOR);
+
+    // Remove os elementos anteriores do container
+    const eventosAnteriores = container.querySelectorAll(EVENT_SELECTOR);
+    eventosAnteriores.forEach(evento => evento.remove());
+
+    // Obtém somente os três primeiros eventos
+    const tresPrimeirosEventos = data.slice(0, 3);
+
+    // Adiciona os novos elementos ao container
+    container.insertAdjacentHTML('beforeend', renderEventos(tresPrimeirosEventos));
+  } catch (error) {
+    console.error(error);
+    const container = document.querySelector(CONTAINER_SELECTOR);
+    container.insertAdjacentHTML('beforeend', '<p>Não foi possível carregar os eventos.</p>');
   }
-  
-  fetchEventos();
-  
+}
+
+function renderEventos(eventos) {
+  return eventos
+    .map(evento => `
+      <article class="evento card p-5 m-3">
+        <h2>${evento.name} - ${new Date(evento.scheduled).toLocaleDateString()}</h2>
+        <h4>${evento.attractions.join(', ')}</h4>
+        <p>${evento.description}</p>
+        <a href="" class="btn btn-primary">reservar ingresso</a>
+      </article>
+    `)
+    .join('');
+}
+
+fetchEventos();
